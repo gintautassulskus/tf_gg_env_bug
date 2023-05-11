f8f5fe6212cabb790ed7660b9bb2d7a9f699942b
Gintautas Sulskus <gintautas.sulskus@gmail.com>
2023-05-11 21:45:15 +0100

    delete old tf file

--- github_env_variables.tf
+++
@@ -1,8 +0,0 @@
-data "github_repository" "repo" {
-  full_name = var.github_repository
-}
-
-resource "github_repository_environment" "repo_environment" {
-  repository  = data.github_repository.repo.name
-  environment = "example_environment"
-}
